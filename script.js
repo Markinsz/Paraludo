ce361306702d6cf82dfe1afdb224db5c8a2243d2
Markinsz <markins.psm@gmail.com>
2024-05-20 17:00:19 -0300

    feat: menu de hambúrguer e formulário feitos

--- script.js
+++ script.js
@@ -8,14 +8,54 @@ function AbrirFecharMenu() {
 
         options.classList.remove("menu_fechado")
 
+        options.style.display = "flex"
+
         iconeX.style.display = "inline"
 
         iconeBarras.style.display = "none"
     } else {
         options.classList.add("menu_fechado")
 
+        options.style.display = "none"
+
         iconeX.style.display = "none"
 
         iconeBarras.style.display = "inline"
     }
+}
+
+const Contato = (event) => {
+    let valorNome = document.getElementById("campo_nome").value
+    let valorEmail = document.getElementById("campo_email").value
+    let valorDescricao = document.getElementById("campo_descricao").value
+
+    console.log(valorNome);
+    console.log(valorEmail);
+    console.log(valorDescricao);
+    
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        descricao: valorDescricao
+    }
+
+    fetch("http://localhost:3000/contato", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dadosForm)
+    })
+    .then(resposta => {
+        console.log(resposta)
+        document.querySelector("#contato form").reset()
+
+        alert("Solicitação Cadastrada")
+    }) 
+    .catch(erro => {
+        console.error(erro)
+        alert("Erro na requisição")
+    })
+
+    event.preventDefault();
 }
